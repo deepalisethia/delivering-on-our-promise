4b7ce757eddccb8c682667aeebe9f78d6934589f
Deepali Sethia <deepali.sethia@gmail.com>
2022-04-14 17:08:14 -0700

    Update

--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -7,38 +7,36 @@ import com.amazon.ata.deliveringonourpromise.types.Order;
 import com.amazon.ata.ordermanipulationauthority.OrderManipulationAuthority;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderDaoTest {
 
     private OrderDao dao;
-    private OrderDao classUnderTest;
+    @BeforeEach
+    private void createDao() {
+        dao = App.getOrderDao();
+    }
 
     @Test
     public void get_forKnownOrderId_returnsOrder() {
         // GIVEN
         String orderId = "900-3746403-0000002";
-        OrderManipulationAuthority service = new OrderManipulationAuthority(OrderDatastore.getDatastore());
-        OrderManipulationAuthorityClient omaClient = new OrderManipulationAuthorityClient(service);
-        dao = new OrderDao(omaClient);
         // WHEN
         Order order = dao.get(orderId);
         // THEN
-        Assertions.assertNotNull(order);
+        assertNotNull(order);
     }
 
     @Test
     public void get_forUnknownOrderId_returnsOrder() {
         // GIVEN
         String orderId = "invalid";
-        OrderManipulationAuthority service = new OrderManipulationAuthority(OrderDatastore.getDatastore());
-        OrderManipulationAuthorityClient omaClient = new OrderManipulationAuthorityClient(service);
-        dao = new OrderDao(omaClient);
         // WHEN
         Order order = dao.get(orderId);
         // THEN
-        Assertions.assertNull(order);
+        assertNull(order);
     }
 
 //    @Test
